8afbe0a7fcf47eb57f22aaa5a88cf5f9c658f2b0
Attila Vigh <demogun@gmail.com>
2020-06-26 08:48:32 +0200

    Modification after relay connection. Still, the open-close works in the oposite way. Database has been hacked last time, so I created some quick liners to ease my work of restoring the structure of DB: /restore_db.sql

--- simple.py
+++ simple.py
@@ -6,28 +6,32 @@ import time
 pwmPin = 17 # Broadcom pin 18 (P1 pin 12)
 ledPin = 23 # Broadcom pin 23 (P1 pin 16)
 
+pin = ['9', '14', '5', '23', '12', '3', '2', '17']
 dc = 95 # duty cycle (0-100) for PWM pin
 
 # Pin Setup:
-GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
-GPIO.setup(ledPin, GPIO.OUT) # LED pin set as output
-GPIO.setup(pwmPin, GPIO.OUT) # PWM pin set as output
-pwm = GPIO.PWM(pwmPin, 50)  # Initialize PWM on pwmPin 100Hz frequency
+#GPIO.setup(ledPin, GPIO.OUT) # LED pin set as output
+#pwm = GPIO.PWM(pwmPin, 50)  # Initialize PWM on pwmPin 100Hz frequency
 
 # Initial state for LEDs:
-GPIO.output(ledPin, GPIO.LOW)
-pwm.start(dc)
+#GPIO.output(ledPin, GPIO.LOW)
+#pwm.start(dc)
 
 print("Here we go! Press CTRL+C to exit")
 try:
-    while 1:
-        time.sleep(3)
-        pwm.ChangeDutyCycle(100-dc)
-        GPIO.output(ledPin, GPIO.HIGH)
-        time.sleep(0.075)
-        GPIO.output(ledPin, GPIO.LOW)
-        time.sleep(0.075)
+    for i in pin:
+        GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+        GPIO.setup(int(i), GPIO.OUT) # PWM pin set as output
+        print("cleaning up: %s", i)
+        GPIO.cleanup()
+#    while 1:
+#        time.sleep(3)
+#        pwm.ChangeDutyCycle(100-dc)
+#        GPIO.output(ledPin, GPIO.HIGH)
+#        time.sleep(0.075)
+#        GPIO.output(ledPin, GPIO.LOW)
+#        time.sleep(0.075)
 except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
-    pwm.stop() # stop PWM
+    #pwm.stop() # stop PWM
     GPIO.cleanup() # cleanup all GPIO
 
